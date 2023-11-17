016c55fda7fcc0ff47f25b349bba7c80656d518b
Dylan Li <hang20145@gmail.com>
2023-11-17 12:29:58 +1100

    Initial Commit

---
+++ AFP_Group2/AFP_Group2App.swift
@@ -0,0 +1,17 @@
+//
+//  AFP_Group2App.swift
+//  AFP_Group2
+//
+//  Created by Dylan on 17/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AFP_Group2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
