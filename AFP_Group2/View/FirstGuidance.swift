602407376be8142376ccf264e7c3a67e7f63aeaa
Jamie <yoko881705@gmail.com>
2023-11-17 13:41:52 +1100

    init Jamie

---
+++ AFP_Group2/View/FirstGuidance.swift
@@ -0,0 +1,20 @@
+//
+//  FirstGuidance.swift
+//  AFP_Group2
+//
+//  Created by Jamie Chao on 17/11/2023.
+//
+
+import SwiftUI
+
+struct FirstGuidance: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FirstGuidance_Previews: PreviewProvider {
+    static var previews: some View {
+        FirstGuidance()
+    }
+}
