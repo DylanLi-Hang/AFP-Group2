4eb37cdde13a4bfdc4c49c0d9204fb17926455bf
Dylan Li <hang20145@gmail.com>
2023-11-20 16:22:08 +1100

    Add navigation bar

--- AFP_Group2/ContentView.swift
+++ AFP_Group2/ContentView.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            TapView()
+                .tabItem {
+                    Label("Home", systemImage: "circle.hexagongrid.fill")
+                }
+            
+            CollectStarView()
+                .tabItem {
+                    Label("Saved", systemImage: "bookmark")
+                }
+            
+            QuoteView()
+                .tabItem {
+                    Label("Profile", systemImage: "person")
+                }
         }
-        .padding()
     }
 }
 
