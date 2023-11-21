a825b8ea80c723d46adafeb4b3d1da859d6aa84c
Jamie <yoko881705@gmail.com>
2023-11-21 11:18:39 +1100

    GuidanceView updated

--- AFP_Group2/View/GuidanceView.swift
+++ AFP_Group2/View/GuidanceView.swift
@@ -9,7 +9,41 @@ import SwiftUI
 
 struct GuidanceView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // Background color
+            Color(.systemBackground).edgesIgnoringSafeArea(.all)
+            
+            
+            
+            // Overlayed focused yellow circle and text
+            VStack {
+                Spacer()
+                
+                Text("Choose your concern")
+                    .font(.title)
+                    .padding(.bottom, 50)
+                
+                Circle()
+                    .fill(Color.yellow.opacity(0.3))
+                    .frame(width: 200, height: 200)
+                    .overlay(
+                    Text("I'm not good at coding")
+                    .font(.headline)
+                    .foregroundColor(.black)
+                    )
+                    .padding(.bottom, 50)
+                                
+                // Hand Icon
+                LottieView(name: "tap", loopMode: .loop)
+                    .frame(width: 100, height: 100)
+                    .padding(.bottom, 20)
+                    .offset(y: -100)
+                
+                Spacer()
+                
+            }
+            
+        }
     }
 }
 
