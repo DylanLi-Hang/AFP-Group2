c614b6a97c847ab6b6c676382eca07b609b09440
Dylan Li <hang20145@gmail.com>
2023-11-22 09:25:11 +1100

    Fix model

---
+++ AFP_Group2/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  AFP_Group2
+//
+//  Created by Dylan on 22/11/2023.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
