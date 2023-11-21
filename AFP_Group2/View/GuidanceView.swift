5ddf67556c30ba9c5e425eed20a7a55660bd42f9
Jamie <yoko881705@gmail.com>
2023-11-21 11:18:13 +1100

    Created and upgated the GuidanView

---
+++ AFP_Group2/View/GuidanceView.swift
@@ -0,0 +1,18 @@
+//
+//  GuidanceView.swift
+//  AFP_Group2
+//
+//  Created by Jamie Chao on 21/11/2023.
+//
+
+import SwiftUI
+
+struct GuidanceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GuidanceView()
+}
---
+++ AFP_Group2/View/GuidanceView2.swift
@@ -0,0 +1,18 @@
+//
+//  GuidanceView2.swift
+//  AFP_Group2
+//
+//  Created by Jamie Chao on 21/11/2023.
+//
+
+import SwiftUI
+
+struct GuidanceView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GuidanceView2()
+}
