d6762010224cc453b05d62ea277996ac2b3f9bb0
Jamie <yoko881705@gmail.com>
2023-11-21 13:51:44 +1100

    Shake and slice animations are done

---
+++ AFP_Group2/View/ShakeView.swift
@@ -0,0 +1,18 @@
+//
+//  ShakeView.swift
+//  AFP_Group2
+//
+//  Created by Jamie Chao on 21/11/2023.
+//
+
+import SwiftUI
+
+struct ShakeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShakeView()
+}
---
+++ AFP_Group2/View/SliceView.swift
@@ -0,0 +1,18 @@
+//
+//  SliceView.swift
+//  AFP_Group2
+//
+//  Created by Jamie Chao on 21/11/2023.
+//
+
+import SwiftUI
+
+struct SliceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SliceView()
+}
