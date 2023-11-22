d1e7f370057228d788b6cd314e24f156eeebd697
Jamie <yoko881705@gmail.com>
2023-11-22 11:55:13 +1100

    Popup animation for guidanceViews

---
+++ AFP_Group2/View/OverlayTest.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  AFP_Group2
+//
+//  Created by Jamie Chao on 22/11/2023.
+//
+
+import SwiftUI
+
+struct OverlayTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OverlayTest()
+}
