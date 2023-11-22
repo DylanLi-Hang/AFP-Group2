3a9724b956e7759190f9568576c690037146fa22
Jamie <yoko881705@gmail.com>
2023-11-22 11:55:53 +1100

    Popup animation for guidanceViews 2

--- AFP_Group2/View/OverlayTest.swift
+++ AFP_Group2/View/OverlayTest.swift
@@ -7,12 +7,58 @@
 
 import SwiftUI
 
-struct OverlayTest: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct OverlayTestView: UIViewRepresentable {
+    var style: UIBlurEffect.Style
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        return UIVisualEffectView(effect: UIBlurEffect(style: style))
+    }
+    
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        uiView.effect = UIBlurEffect(style: style)
     }
 }
 
-#Preview {
-    OverlayTest()
+struct OverlayView: View {
+    @State private var isActive: Bool = false
+    var body: some View {
+        NavigationView {
+            
+            ZStack {
+                
+                // The overlay with blur effect
+                OverlayTestView(style: .systemMaterialDark)
+                    .edgesIgnoringSafeArea(.all) 
+                // Ensures the blur goes edge-to-edge
+                
+                // Any other views you want on top of the blur
+                Text("Future BubbleView")
+                    .foregroundColor(.white)
+                    .font(.title)
+                
+                NavigationLink("", destination: GuidanceView(), isActive: $isActive)
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                            isActive = true
+                        }
+                    }
+                    .hidden()
+            }
+        }
+    }
 }
+//    private func delayText() {
+//        // Delay of 7.5 seconds
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+//            hasTimeElapsed = true
+//        }
+        
+  //  }
+
+struct OverlayView_Previews: PreviewProvider {
+    static var previews: some View {
+        OverlayView()
+    }
+}
+
+
